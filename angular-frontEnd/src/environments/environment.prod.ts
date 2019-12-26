09a7193e882bf5ecb69e069c05169339c4547b26
tigelah <fernando-guide@hotmail.com>
2019-12-26 12:13:50 -0300

    deploy on heroku

--- angular-frontEnd/src/environments/environment.prod.ts
+++ angular-frontEnd/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:8080/api/v1/employees'
+  baseUrl: 'https://spring-mongodb-backend.herokuapp.com/api/v1/employees'
 };
