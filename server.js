357710e1becd623697cbc04a98c7583728e95e85
tigelah <fernando-guide@hotmail.com>
2019-12-26 13:45:15 -0300

    deploy

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 //Importa as dependências que acabamos de instalar
 const express = require("express");
 const path = require("path");
-
 const app = express();
-
 // Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
 app.use(express.static(__dirname + "/dist/angular-frontend"));
-
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname + "/dist/angular-frontend/index.html"));
 });
-
 // Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
