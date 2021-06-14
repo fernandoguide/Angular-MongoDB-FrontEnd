ec35a30afcc5cd73813ecb9d491da1d5fd12f0fe
Fernando Oliveira <39173306+fernandoguide@users.noreply.github.com>
2021-06-14 09:18:30 -0300

    Update employee.service.ts

--- src/app/service/employee.service.ts
+++ src/app/service/employee.service.ts
@@ -16,10 +16,10 @@ export class EmployeeService {
   getEmployee(id: number): Observable<any> {
     return this.http.get(`${environment.baseUrl}/${id}`);
   }
-  createEmployee(employee: any): Observable<Object> {
+  createEmployee(employee: any): Observable<Any> {
     return this.http.post(`${environment.baseUrl}`, employee);
   }
-  updateEmployee(id: number, employee: any): Observable<Object> {
+  updateEmployee(id: number, employee: any): Observable<Any> {
     return this.http.put(`${environment.baseUrl}/${id}`, employee);
   }
   deleteEmployee(id: number): Observable<any> {
